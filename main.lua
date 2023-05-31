de0a8e9c5b6938ae76ea3172a0ba64c9e4900f91
7GrandFan <119379698+TheMagicFan@users.noreply.github.com>
2023-05-31 14:14:52 +0800

    moved soem stuff to utility

--- main.lua
+++ main.lua
@@ -18,7 +18,7 @@ Section:NewButton("check it", "check it rn.", function()
     printidentity()
 end)
 
-local Section2 = Tab:NewSection("Utlity")
+local Section2 = Tab:NewSection("Utlity / Admin")
 
 Section2:NewButton("Dark Dex", "Load Dark Dex", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Babyhamsta/RBLX_Scripts/main/Universal/BypassedDarkDexV3.lua", true))()
@@ -28,38 +28,36 @@ Section2:NewButton("Remote2Script", "simple remotespy", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/TheMagicFan/backup-sh-t/main/Remote2script.lua", true))()
 end)
 
-local Section3 = Tab:NewSection("Admin Scripts")
-
-Section3:NewButton("Infinite yield", "info", function()
+Section2:NewButton("Infinite yield", "info", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source", true))()
 end)
 
-Section3:NewButton("Cmd X", "info", function()
+Section2:NewButton("Cmd X", "info", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/CMD-X/CMD-X/master/Source", true))()
 end)
 
-local Section4 = Tab:NewSection("Troll Scripts")
+local Section3 = Tab:NewSection("Troll Scripts")
 
-Section4:NewButton("ball", "info", function()
+Section3:NewButton("ball", "info", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/TheMagicFan/Ball/main/ball.lua", true))()
 end)
 
-Section4:NewButton("Walk on Walls", "info", function()
+Section3:NewButton("Walk on Walls", "info", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/TheMagicFan/walk-on-walls/main/WalkOnWalls.lua)"))()
 end)
 
-local Section5 = Tab:NewSection("Keybind")
+local Section4 = Tab:NewSection("Keybind")
 
-Section5:NewKeybind("KeybindText", "KeybindInfo", Enum.KeyCode.M, function()
+Section4:NewKeybind("KeybindText", "KeybindInfo", Enum.KeyCode.M, function()
 	Library:ToggleUI()
 end)
 
 local Section6 = Tab:NewSection("Local Player Scripts")
 
-Section6:NewSlider("Walkspeed", "SliderInfo", 500, 16, function(s) -- 500 (MaxValue) | 0 (MinValue)
+Section5:NewSlider("Walkspeed", "SliderInfo", 500, 16, function(s) -- 500 (MaxValue) | 0 (MinValue)
     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
 end)
 
-Section6:NewSlider("JumpPower", "SliderInfo", 500, 50, function(a) -- 500 (MaxValue) | 0 (MinValue)
+Section5:NewSlider("JumpPower", "SliderInfo", 500, 50, function(a) -- 500 (MaxValue) | 0 (MinValue)
     game.Players.LocalPlayer.Character.Humanoid.JumpPower = a
 end)
