aede85ae6908d2a17a2a760a370b1b1bbfc369ab
7GrandFan <119379698+TheMagicFan@users.noreply.github.com>
2023-05-31 13:38:52 +0800

    rewrite

--- main.lua
+++ main.lua
@@ -1,105 +1,60 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("Fe Scripthub - Universal / Public v4.10-3", "BloodTheme")
-local Tab = Window:NewTab("FE Scripts/GUIs")
-local Tab2 = Window:NewTab("Hubs/GUIs")
+local Window = Library.CreateLib("Universal FE / Rewrite", "ocean")
 
--- buttons section
-local Section = Tab:NewSection("Level 6 - 8 Executors Required")
+-- Windows
 
--- buttons
-Section:NewButton("check level of executor", "CHECK IT RIGHT NOW BI-", function()
-	printidentity()
+local Tab = Window:NewTab("Universal Scripts")
+
+-- REWRITE JAJJAJAJAJ
+
+local Section = Tab:NewSection("Check Level of Executor (lvl 6 - 8 only.)")
+
+Section:NewButton("check it", "check it rn.", function()
+    printidentity()
 end)
 
-local UtilsSection = Tab:NewSection("Utility")
+local Section2 = Tab:NewSection("Utlity")
 
-UtilsSection:NewButton("Load Inf Yield", "lul", function()
-	loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+Section2:NewButton("Dark Dex", "Load Dark Dex", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Babyhamsta/RBLX_Scripts/main/Universal/BypassedDarkDexV3.lua", true))()
 end)
 
-UtilsSection:NewButton("Load Dark Dex", "synapse script", function()
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/Babyhamsta/RBLX_Scripts/main/Universal/BypassedDarkDexV3.lua", true))()
+Section2:NewButton("Remote2Script", "simple remotespy", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/TheMagicFan/backup-sh-t/main/Remote2script.lua", true))()
 end)
 
-UtilsSection:NewButton("Load R2S", "remote spy", function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/scotdotwtf/scriptsforvideos1/main/r2s.lua"))()
+local Section3 = Tab:NewSection("Admin Scripts")
+
+Section3:NewButton("Infinite yield", "info", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source", true))()
 end)
 
-local LocalPlayerSection = Tab:NewSection("LocalPlayer")
-
-LocalPlayerSection:NewButton("walkspeed", "Makes you walk faster.", function()
-	LocalPlayerSection:NewSlider("WalkSpeed value", "Makes you walk faster.", 500, 16, function(s) -- 500 (MaxValue) | 0 (MinValue)
-        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
-    end)
+Section3:NewButton("Cmd X", "info", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CMD-X/CMD-X/master/Source", true))()
 end)
 
-LocalPlayerSection:NewButton("JumpPower", "Makes you jump higher.", function()
-	Section:NewSlider("JumpPower value", "Makes you jump higher.", 500, 50, function(s) -- 500 (MaxValue) | 0 (MinValue)
-        game.Players.LocalPlayer.Character.Humanoid.JumpPower = s
-    end)
+local Section4 = Tab:NewSection("Troll Scripts")
+
+Section4:NewButton("ball", "info", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/TheMagicFan/Ball/main/ball.lua", true))()
 end)
 
-local OthersSection = Tab:NewSection("Others")
-
-OthersSection:NewButton("Load Vape V4", "bedwars", function()
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))()
+Section4:NewButton("Walk on Walls", "info", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/TheMagicFan/walk-on-walls/main/WalkOnWalls.lua)"))()
 end)
 
-OthersSection:NewButton("Load Oblivion", "random script lmao", function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/CorruptOblivion/Oblivion/main/loader.lua"))()
-end)
+local Section5 = Tab:NewSection("Keybind")
 
-OthersSection:NewButton("18+ :smirk:", "no", function()
-    print("why click")
-end)
-
-OthersSection:NewButton("R15 Emotes", "gives all r15 emotes", function()   
-    loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Gi7331/scripts/main/Emote.lua"))()
-end)
-
-OthersSection:NewButton("Silent Aim", "silent aim" function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Averiias/Universal-SilentAim/main/main.lua"))()
-end)
-
-local TrollSection = Tab:NewSection("Troll Scripts :troll:")
-
-TrollSection:NewButton("ball", "puts your character inside a ball", function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/TheMagicFan/Ball/main/ball.lua"))()
-end)
-
-TrollSection:NewButton("Walk on Walls", "makes you walk on walls", function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/TheMagicFan/walk-on-walls/main/WalkOnWalls.lua"))()
-end)
-
-
-
--- keybinds (imagine if i make it for buttons lmao)
-
-local KeybindSection = Tab:NewSection("keybind")
-
-KeybindSection:NewKeybind("KeybindText", "KeybindInfo", Enum.KeyCode.M, function()
+Section5:NewKeybind("KeybindText", "KeybindInfo", Enum.KeyCode.M, function()
 	Library:ToggleUI()
 end)
 
--- hubs and stuff
+Local Section6 = Tab:NewSection("Local Player Scripts")
 
-local HubSection = Tab2:NewSection("Hubs")
-
-
--- script hubs
-
-HubSection:NewButton("Universal FE Hub", "info", function()
-	loadstring(game:HttpGet('https://raw.githubusercontent.com/Dvrknvss/UniversalFEScriptHub/main/Script'))()
+Section6:NewSlider("Walkspeed", "SliderInfo", 500, 16, function(s) -- 500 (MaxValue) | 0 (MinValue)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
 end)
 
-HubSection:NewButton("Owl Hub", "info", function()
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/CriShoux/OwlHub/master/OwlHub.txt"))()
-end)
-
-HubSection:NewButton("Syntax V3 (key is SyntaxeV3Free)", "info" function()
-    loadstring(game:HttpGet("https://pastebin.com/raw/Vubm8tdf"))()
-end)
-
-HubSection:NewButton("Simplity V2", "info", function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/HeyGyt/simplityv2/main/main"))()
+Section6:NewSlider("JumpPower", "SliderInfo", 500, 50, function(a) -- 500 (MaxValue) | 0 (MinValue)
+    game.Players.LocalPlayer.Character.Humanoid.JumpSpeed = a
 end)
